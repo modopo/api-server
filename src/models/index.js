1b7a01d133cf3a91400d72ae42d3cc8e7fba230c
Jonathan Tsai <modopo@gmail.com>
2023-04-05 15:54:38 -0700

    configs

--- src/models/index.js
+++ src/models/index.js
@@ -2,6 +2,7 @@
 
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize(`postgres://${process.env.DB_USER}:${process.env.DB_PASS}@localhost:5432/db_401`)
+const SQL_URL = process.env.DB_LOCAL_URL || process.env.DB_URL;
 
+const sequelize = new Sequelize(SQL_URL);
 module.exports = sequelize;
