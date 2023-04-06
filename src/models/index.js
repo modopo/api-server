d834098cbe6c6303599124abec4a5c2d387ce42a
Jonathan Tsai <modopo@gmail.com>
2023-04-06 09:11:27 -0700

    cloud server only

--- src/models/index.js
+++ src/models/index.js
@@ -2,7 +2,7 @@
 
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const SQL_URL = process.env.DB_LOCAL_URL || process.env.DB_URL;
+const SQL_URL = process.env.DB_URL;
 
 const sequelize = new Sequelize(SQL_URL);
 module.exports = sequelize;
