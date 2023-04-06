74f0e3696e38f315728ee70ea2a072f5b9209a5c
Jonathan Tsai <modopo@gmail.com>
2023-04-06 13:19:11 -0700

    revoked cloud db url

--- src/models/index.js
+++ src/models/index.js
@@ -3,6 +3,5 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const SQL_LOCAL_URL = process.env.DB_URL;
-const SQL_CLOUD_URL = 'postgres://db_401_user:sc5U5L73Rx4NDd6h8RXols77AUq6ou5I@dpg-cgmvhg5269v6fns0vd70-a.oregon-postgres.render.com/db_401'
-const sequelize = new Sequelize(SQL_LOCAL_URL || SQL_CLOUD_URL) ;
+const sequelize = new Sequelize(SQL_LOCAL_URL) ;
 module.exports = sequelize;
